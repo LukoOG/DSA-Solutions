7089abc574d73e42533ca111a30b0b049c57a5c8
LukoOG <emmanueladesipe01@gmail.com>
2026-04-13 14:38:29 +0100

    feat: solved FizzBuzz in TypeScript

---
+++ Misc/FizzBuzz/TS/fizzBuzz.ts
@@ -0,0 +1,28 @@
+function fizzBuzz(n: number){
+    let results = [];
+
+    for(let i = 1; i <= n; i++){
+        const divisibleBy3 = i % 3 === 0;
+        const divisibleBy5 = i % 5 === 0;
+
+        if(divisibleBy3 && divisibleBy5){
+            results.push("FizzBuzz")
+        } else if(divisibleBy3){
+            results.push("Fizz")
+        } else if(divisibleBy5) {
+            results.push("Buzz")
+        } else{
+            results.push(i)
+        }
+    }
+
+    return results
+}
+
+const testCases: number[] = [1, 3, 5, 15, 16];
+
+for (const n of testCases) {
+    console.log(`Input:       ${n}`);
+    console.log(`Output:      ${JSON.stringify(fizzBuzz(n))}`);
+    console.log("-".repeat(35));
+}
