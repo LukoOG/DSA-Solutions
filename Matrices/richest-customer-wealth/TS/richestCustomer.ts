faa5ad3c706faecb9130d262cf6770afcfd702d1
LukoOG <emmanueladesipe01@gmail.com>
2026-04-13 11:52:03 +0100

    feat: cleaned running wealth TS problem

--- Matrices/richest-customer-wealth/TS/richestCustomer.ts
+++ Matrices/richest-customer-wealth/TS/richestCustomer.ts
@@ -1,14 +1,14 @@
 export {} //to turn each TS file into it's own module
 
 function richestCustomer(accounts: number[][]){
-    let maxWealthSoFar = 0;
+    let maxWealth = 0;
     for (const account of accounts){
-        let currentMaxWealth = account.reduce((val1, val2)=>val1+val2, 0)
+        let currentMaxWealth = account.reduce((val1, val2) => val1 + val2, 0)
 
-        maxWealthSoFar = Math.max(maxWealthSoFar, currentMaxWealth)
+        maxWealth = Math.max(maxWealth, currentMaxWealth)
     }
 
-    return maxWealthSoFar
+    return maxWealth
 }
 
 
@@ -21,10 +21,16 @@ const testCases = [
         [[1, 5], [7, 3], [3, 5]],      // expected: 10
         [[2, 8, 7], [7, 1, 3], [1, 9, 5]],  // expected: 17
     ]
-
     
 for(const testCase of testCases){
     console.log(`Input:             ${JSON.stringify(testCase)}`)
     console.log(`Solution:          ${JSON.stringify(richestCustomer(testCase))}`)
     console.log("_".repeat(35))
+}
+
+//this is a concise solution given from GPT
+function richestCustomerGPT(accounts: number[][]): number {
+    return Math.max(
+        ...accounts.map(acc => acc.reduce((a, b) => a + b, 0))
+    );
 }
