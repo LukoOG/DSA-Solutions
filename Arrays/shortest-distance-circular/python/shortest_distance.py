a9203ba2c6d5be01635c42392bca1ea8f1061887
LukoOG <emmanueladesipe01@gmail.com>
2026-04-15 14:37:19 +0100

    feat: solved Shortest distance to target in circular array in Python

--- Arrays/shortest-distance-circular/python/shortest_distance.py
+++ Arrays/shortest-distance-circular/python/shortest_distance.py
@@ -1,10 +1,16 @@
+def minimum_dist(targetidx: int, startidx: int, length: int)->int:
+    return min(abs(targetidx - startidx), length - abs(startidx - targetidx))
+
 class Solution:
     def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
         valid_idxs =[]
         for idx, word in enumerate(words):
             if word == target:
                 valid_idxs.append(idx)
-        map(lambda x: min(x, 9), valid_idxs)
+        if not valid_idxs:
+            return -1
+        
+        return min(map(lambda valid_idx: minimum_dist(valid_idx, startIndex, len(words)), valid_idxs))
         
                 
     
@@ -17,6 +23,7 @@ if __name__ == "__main__":
         (["hello", "i", "am", "leetcode", "here"], "hello", 1),     # → 1, wraps around
         (["a", "b", "leetcode"], "leetcode", 0),                    # → 1, wraps backward
         (["a"], "a", 0),                                            # → 0, single element
+        (["i","eat","leetcode"], "ate", 0),
     ]
 
     for words, target, startIndex in test_cases:
