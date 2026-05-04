faedbdefd2aa9b69e099825a2ab819c49a7b9248
LukoOG <emmanueladesipe01@gmail.com>
2026-05-04 11:34:20 +0100

    feat: Solved rotate Image in typescript. Added README

--- Matrices/rotate_image/TS/rotateImage.ts
+++ Matrices/rotate_image/TS/rotateImage.ts
@@ -1,31 +1,55 @@
 export {};
 
-function rotate(matrix: number[][]): void{
-    matrix.forEach((e, i) => {
-        console.log(`index ${i} for element ${e}`)
-    })
+function rotate(matrix: number[][]): void {
+  // if(matrix.length !== matrix[0].length) return;
+  const n = matrix.length - 1;
+
+  let lastRow: number[] = [];
+  const copy = matrix.map(r => [...r]);
+
+  copy.forEach((row, r) => {
+    let newRow: number[] = [];
+    for (let i = n; i >= 0; i--) {
+        newRow.push(copy[i][r]);
+    }
+    matrix[r] = newRow;
+    return row;
+  });
 }
 
 const testCases: number[][][] = [
-    [[1, 2, 3],
-     [4, 5, 6],
-     [7, 8, 9]],
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
 
-    [[5, 1, 9, 11],
-     [2, 4, 8, 10],
-     [13, 3, 6, 7],
-     [15, 14, 12, 16]],
+  [
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16],
+  ],
 
-    [[1]],
+  [[1]],
 
-    [[1, 2],
-     [3, 4]],
+  [
+    [1, 2],
+    [3, 4],
+  ],
 ];
 
-for (const matrix of testCases) {
-    const original = matrix.map(row => [...row]);
-    rotate(matrix);
-    console.log(`Input:       ${JSON.stringify(original)}`);
-    console.log(`Output:      ${JSON.stringify(matrix)}`);
-    console.log("-".repeat(35));
-}
+// for (const matrix of testCases) {
+//   const original = matrix.map((row) => [...row]);
+//   rotate(matrix);
+//   console.log(`Input:       ${JSON.stringify(original)}`);
+//   console.log(`Output:      ${JSON.stringify(matrix)}`);
+//   console.log("-".repeat(35));
+// }
+
+const test = testCases[1]
+const original = test.map((row) => [...row]);
+rotate(test);
+console.log(`Input:       ${JSON.stringify(original)}`);
+console.log(`Output:      ${JSON.stringify(test)}`);
+console.log("-".repeat(35));
